fde96975e84e5607b1eb0ea542bb276a88c72f08
jonathanemserh <jonathanemserh@gmail.com>
2026-03-26 01:25:52 +0000

    fix(build): restaura todos os modelos no schema prisma

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "37da80a77140b330"; }
+/******/ 		__webpack_require__.h = function() { return "4ff9829864345646"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
